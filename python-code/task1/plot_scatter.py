8480e63eacf4f435f0f6f91a227aba871d9a2ef7
Ali <1932428206@qq.com>
2022-12-23 14:47:03 +0800

    2

--- python-code/task1/plot_scatter.py
+++ python-code/task1/plot_scatter.py
@@ -17,16 +17,23 @@ order_loc = order_loc.values
 
 plt.rcParams['font.sans-serif'] = [u'SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-plt.scatter(aunt_loc[:, 0], aunt_loc[:, 1], marker='o', linewidths=0.1)
-plt.title('阿姨分布图')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.savefig('../../pic/aunt.png')
-plt.show()
+# plt.scatter(aunt_loc[:, 0], aunt_loc[:, 1], marker='o', linewidths=0.1)
+# plt.title('阿姨分布图')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.savefig('../../pic/aunt.png')
+# plt.show()
 
-plt.scatter(order_loc[:, 0], order_loc[:, 1], c='red', marker='^', linewidths=0.1)
-plt.title('订单分布图')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.savefig('../../pic/order.png')
-plt.show()
+order['serviceUnitTime'] = order['serviceUnitTime'] / 60
+order['serviceLastTime'] = (order['serviceLastTime'] - order['serviceFirstTime']) / 3600
+order['serviceFirstTime'] = (order['serviceFirstTime'] - min(order['serviceFirstTime'])) / 3600
+
+marker = ['o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
+for i in range(13):
+    plt.scatter(order[order['serviceFirstTime'] == i].iloc[:, 5], order[order['serviceFirstTime'] == i].iloc[:, 6],
+                c='red', marker=marker[i], linewidths=0.1, label=f'订单Starttime={i}')
+    plt.title('订单分布图')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.savefig(f'../../pic/order_starttime分布图{i}.png')
+    plt.show()
